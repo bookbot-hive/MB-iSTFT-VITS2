0c2d337c1f0bb311473e02dc49543e38a988f709
w11wo <wilsonwong961@gmail.com>
2023-09-08 11:26:19 +0700

    BI IPA Support

--- text/cleaners.py
+++ text/cleaners.py
@@ -1,32 +1,62 @@
 import re
-from text.japanese import japanese_to_romaji_with_accent, japanese_to_ipa, japanese_to_ipa2, japanese_to_ipa3
-from text.korean import latin_to_hangul, number_to_hangul, divide_hangul, korean_to_lazy_ipa, korean_to_ipa
+from text.japanese import (
+    japanese_to_romaji_with_accent,
+    japanese_to_ipa,
+    japanese_to_ipa2,
+    japanese_to_ipa3,
+)
+from text.korean import (
+    latin_to_hangul,
+    number_to_hangul,
+    divide_hangul,
+    korean_to_lazy_ipa,
+    korean_to_ipa,
+)
 from g2pk2 import G2p
-from text.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
-#from text.sanskrit import devanagari_to_ipa
-from text.english import english_to_ipa, english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
-#from text.thai import num_to_thai, latin_to_thai
-#from text.shanghainese import shanghainese_to_ipa
-#from text.cantonese import cantonese_to_ipa
-#from text.ngu_dialect import ngu_dialect_to_ipa
+from text.mandarin import (
+    number_to_chinese,
+    chinese_to_bopomofo,
+    latin_to_bopomofo,
+    chinese_to_romaji,
+    chinese_to_lazy_ipa,
+    chinese_to_ipa,
+    chinese_to_ipa2,
+)
+
+# from text.sanskrit import devanagari_to_ipa
+from text.english import (
+    english_to_ipa,
+    english_to_lazy_ipa,
+    english_to_ipa2,
+    english_to_lazy_ipa2,
+)
+
+# from text.thai import num_to_thai, latin_to_thai
+# from text.shanghainese import shanghainese_to_ipa
+# from text.cantonese import cantonese_to_ipa
+# from text.ngu_dialect import ngu_dialect_to_ipa
 from unidecode import unidecode
 from phonemizer import phonemize
+from g2p_id import G2p
+
+g2p = G2p()
 
 
-_whitespace_re = re.compile(r'\s+')
+_whitespace_re = re.compile(r"\s+")
 
 
 def collapse_whitespace(text):
-    return re.sub(_whitespace_re, ' ', text)
+    return re.sub(_whitespace_re, " ", text)
 
 
 def convert_to_ascii(text):
     return unidecode(text)
 
-#- For replication of https://github.com/FENRlR/MB-iSTFT-VITS2/issues/2
+
+# - For replication of https://github.com/FENRlR/MB-iSTFT-VITS2/issues/2
 # you may need to replace the symbol to Russian one
 def basic_cleaners(text):
-    '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
+    """Basic pipeline that lowercases and collapses whitespace without transliteration."""
     text = text.lower()
     text = collapse_whitespace(text)
     return text
@@ -36,8 +66,12 @@ def fix_g2pk2_error(text):
     new_text = ""
     i = 0
     while i < len(text) - 4:
-        if (text[i:i+3] == 'ㅇㅡㄹ' or text[i:i+3] == 'ㄹㅡㄹ') and text[i+3] == ' ' and text[i+4] == 'ㄹ':
-            new_text += text[i:i+3] + ' ' + 'ㄴ'
+        if (
+            (text[i : i + 3] == "ㅇㅡㄹ" or text[i : i + 3] == "ㄹㅡㄹ")
+            and text[i + 3] == " "
+            and text[i + 4] == "ㄹ"
+        ):
+            new_text += text[i : i + 3] + " " + "ㄴ"
             i += 5
         else:
             new_text += text[i]
@@ -48,113 +82,160 @@ def fix_g2pk2_error(text):
 
 
 def english_cleaners(text):
-  return english_to_ipa(text)
+    return english_to_ipa(text)
 
 
 def english_cleaners2(text):
-  return english_to_ipa2(text)
+    return english_to_ipa2(text)
 
 
 def japanese_cleaners(text):
     text = japanese_to_romaji_with_accent(text)
-    text = re.sub(r'([A-Za-z])$', r'\1.', text)
+    text = re.sub(r"([A-Za-z])$", r"\1.", text)
     return text
 
 
 def japanese_cleaners2(text):
-    return japanese_cleaners(text).replace('ts', 'ʦ').replace('...', '…')
+    return japanese_cleaners(text).replace("ts", "ʦ").replace("...", "…")
 
 
 def korean_cleaners(text):
-    '''Pipeline for Korean text'''
+    """Pipeline for Korean text"""
     text = latin_to_hangul(text)
     g2p = G2p()
     text = g2p(text)
     text = divide_hangul(text)
     text = fix_g2pk2_error(text)
-    text = re.sub(r'([\u3131-\u3163])$', r'\1.', text)
+    text = re.sub(r"([\u3131-\u3163])$", r"\1.", text)
     return text
 
 
 def chinese_cleaners(text):
-    '''Pipeline for Chinese text'''
+    """Pipeline for Chinese text"""
     text = number_to_chinese(text)
     text = chinese_to_bopomofo(text)
     text = latin_to_bopomofo(text)
-    text = re.sub(r'([ˉˊˇˋ˙])$', r'\1。', text)
+    text = re.sub(r"([ˉˊˇˋ˙])$", r"\1。", text)
     return text
 
 
 def zh_ja_mixture_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_romaji(x.group(1))+' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]', lambda x: japanese_to_romaji_with_accent(
-        x.group(1)).replace('ts', 'ʦ').replace('u', 'ɯ').replace('...', '…')+' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    text = re.sub(
+        r"\[ZH\](.*?)\[ZH\]", lambda x: chinese_to_romaji(x.group(1)) + " ", text
+    )
+    text = re.sub(
+        r"\[JA\](.*?)\[JA\]",
+        lambda x: japanese_to_romaji_with_accent(x.group(1))
+        .replace("ts", "ʦ")
+        .replace("u", "ɯ")
+        .replace("...", "…")
+        + " ",
+        text,
+    )
+    text = re.sub(r"\s+$", "", text)
+    text = re.sub(r"([^\.,!\?\-…~])$", r"\1.", text)
     return text
 
 
 def sanskrit_cleaners(text):
-    text = text.replace('॥', '।').replace('ॐ', 'ओम्')
-    text = re.sub(r'([^।])$', r'\1।', text)
+    text = text.replace("॥", "।").replace("ॐ", "ओम्")
+    text = re.sub(r"([^।])$", r"\1।", text)
     return text
 
 
 def kej_cleaners(text):
-    text = re.sub(r'\[KO\](.*?)\[KO\]',
-                  lambda x: korean_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[EN\](.*?)\[EN\]',
-                  lambda x: english_to_ipa2(x.group(1)) + ' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]',
-                  lambda x: japanese_to_ipa2(x.group(1)) + ' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    text = re.sub(r"\[KO\](.*?)\[KO\]", lambda x: korean_to_ipa(x.group(1)) + " ", text)
+    text = re.sub(
+        r"\[EN\](.*?)\[EN\]", lambda x: english_to_ipa2(x.group(1)) + " ", text
+    )
+    text = re.sub(
+        r"\[JA\](.*?)\[JA\]", lambda x: japanese_to_ipa2(x.group(1)) + " ", text
+    )
+    text = re.sub(r"\s+$", "", text)
+    text = re.sub(r"([^\.,!\?\-…~])$", r"\1.", text)
     return text
 
 
 def cjks_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_lazy_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]',
-                  lambda x: japanese_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[KO\](.*?)\[KO\]',
-                  lambda x: korean_to_lazy_ipa(x.group(1))+' ', text)
-    #text = re.sub(r'\[SA\](.*?)\[SA\]',
+    text = re.sub(
+        r"\[ZH\](.*?)\[ZH\]", lambda x: chinese_to_lazy_ipa(x.group(1)) + " ", text
+    )
+    text = re.sub(
+        r"\[JA\](.*?)\[JA\]", lambda x: japanese_to_ipa(x.group(1)) + " ", text
+    )
+    text = re.sub(
+        r"\[KO\](.*?)\[KO\]", lambda x: korean_to_lazy_ipa(x.group(1)) + " ", text
+    )
+    # text = re.sub(r'\[SA\](.*?)\[SA\]',
     #              lambda x: devanagari_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[EN\](.*?)\[EN\]',
-                  lambda x: english_to_lazy_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    text = re.sub(
+        r"\[EN\](.*?)\[EN\]", lambda x: english_to_lazy_ipa(x.group(1)) + " ", text
+    )
+    text = re.sub(r"\s+$", "", text)
+    text = re.sub(r"([^\.,!\?\-…~])$", r"\1.", text)
     return text
+
 
 def cjke_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]', lambda x: chinese_to_lazy_ipa(x.group(1)).replace(
-        'ʧ', 'tʃ').replace('ʦ', 'ts').replace('ɥan', 'ɥæn')+' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]', lambda x: japanese_to_ipa(x.group(1)).replace('ʧ', 'tʃ').replace(
-        'ʦ', 'ts').replace('ɥan', 'ɥæn').replace('ʥ', 'dz')+' ', text)
-    text = re.sub(r'\[KO\](.*?)\[KO\]',
-                  lambda x: korean_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[EN\](.*?)\[EN\]', lambda x: english_to_ipa2(x.group(1)).replace('ɑ', 'a').replace(
-        'ɔ', 'o').replace('ɛ', 'e').replace('ɪ', 'i').replace('ʊ', 'u')+' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    text = re.sub(
+        r"\[ZH\](.*?)\[ZH\]",
+        lambda x: chinese_to_lazy_ipa(x.group(1))
+        .replace("ʧ", "tʃ")
+        .replace("ʦ", "ts")
+        .replace("ɥan", "ɥæn")
+        + " ",
+        text,
+    )
+    text = re.sub(
+        r"\[JA\](.*?)\[JA\]",
+        lambda x: japanese_to_ipa(x.group(1))
+        .replace("ʧ", "tʃ")
+        .replace("ʦ", "ts")
+        .replace("ɥan", "ɥæn")
+        .replace("ʥ", "dz")
+        + " ",
+        text,
+    )
+    text = re.sub(r"\[KO\](.*?)\[KO\]", lambda x: korean_to_ipa(x.group(1)) + " ", text)
+    text = re.sub(
+        r"\[EN\](.*?)\[EN\]",
+        lambda x: english_to_ipa2(x.group(1))
+        .replace("ɑ", "a")
+        .replace("ɔ", "o")
+        .replace("ɛ", "e")
+        .replace("ɪ", "i")
+        .replace("ʊ", "u")
+        + " ",
+        text,
+    )
+    text = re.sub(r"\s+$", "", text)
+    text = re.sub(r"([^\.,!\?\-…~])$", r"\1.", text)
     return text
+
 
 def cjke_cleaners2(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]',
-                  lambda x: japanese_to_ipa2(x.group(1))+' ', text)
-    text = re.sub(r'\[KO\](.*?)\[KO\]',
-                  lambda x: korean_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[EN\](.*?)\[EN\]',
-                  lambda x: english_to_ipa2(x.group(1))+' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    text = re.sub(
+        r"\[ZH\](.*?)\[ZH\]", lambda x: chinese_to_ipa(x.group(1)) + " ", text
+    )
+    text = re.sub(
+        r"\[JA\](.*?)\[JA\]", lambda x: japanese_to_ipa2(x.group(1)) + " ", text
+    )
+    text = re.sub(r"\[KO\](.*?)\[KO\]", lambda x: korean_to_ipa(x.group(1)) + " ", text)
+    text = re.sub(
+        r"\[EN\](.*?)\[EN\]", lambda x: english_to_ipa2(x.group(1)) + " ", text
+    )
+    text = re.sub(r"\s+$", "", text)
+    text = re.sub(r"([^\.,!\?\-…~])$", r"\1.", text)
     return text
 
-'''
+
+def indonesian_cleaners(text):
+    phonemes = g2p(text)
+    phonemes = "# #".join(["#".join(phn) for phn in phonemes])
+    return phonemes
+
+
+"""
 def thai_cleaners(text):
     text = num_to_thai(text)
     text = latin_to_thai(text)
@@ -183,4 +264,4 @@ def chinese_dialect_cleaners(text):
     text = re.sub(r'\s+$', '', text)
     text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
     return text
-'''
+"""
--- text/symbols.py
+++ text/symbols.py
@@ -1,96 +1,126 @@
-'''
+"""
 Defines the set of symbols used in text input to the model.
-'''
-'''
+"""
+"""
 # english_cleaners
 _pad        = '_'
 _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
-'''
+"""
 
-'''# japanese_cleaners
+"""# japanese_cleaners
 _pad        = '_'
 _punctuation = ',.!?-'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧ↓↑ '
-'''
+"""
 
-'''
+"""
 # japanese_cleaners2
 _pad        = '_'
 _punctuation = ',.!?-~…'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
-'''
+"""
 
-'''
+"""
 # korean_cleaners
 _pad        = '_'
 _punctuation = ',.!?…~'
 _letters = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅓㅗㅜㅡㅣㅐㅔ '
-'''
+"""
 
-'''# chinese_cleaners
+"""# chinese_cleaners
 _pad        = '_'
 _punctuation = '，。！？—…'
 _letters = 'ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦㄧㄨㄩˉˊˇˋ˙ '
-'''
+"""
 
-'''# zh_ja_mixture_cleaners
+"""# zh_ja_mixture_cleaners
 _pad        = '_'
 _punctuation = ',.!?-~…'
 _letters = 'AEINOQUabdefghijklmnoprstuvwyzʃʧʦɯɹəɥ⁼ʰ`→↓↑ '
-'''
+"""
 
-'''# sanskrit_cleaners
+"""# sanskrit_cleaners
 _pad        = '_'
 _punctuation = '।'
 _letters = 'ँंःअआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलळवशषसहऽािीुूृॄेैोौ्ॠॢ '
-'''
+"""
 
-'''# cjks_cleaners
+"""# cjks_cleaners
 _pad        = '_'
 _punctuation = ',.!?-~…'
 _letters = 'NQabdefghijklmnopstuvwxyzʃʧʥʦɯɹəɥçɸɾβŋɦː⁼ʰ`^#*=→↓↑ '
-'''
+"""
 
-'''# thai_cleaners
+"""# thai_cleaners
 _pad        = '_'
 _punctuation = '.!? '
 _letters = 'กขฃคฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลวศษสหฬอฮฯะัาำิีึืุูเแโใไๅๆ็่้๊๋์'
-'''
+"""
 
 
-_pad        = '_'
-_punctuation = ',.!?-~…'
-_letters = 'NQabdefghijklmnopstuvwxyzɑæʃʑçɯɪɔɛɹðəɫɥɸʊɾʒθβŋɦ⁼ʰ`^#*=ˈˌ→↓↑ '
+_pad = "_"
+_punctuation = ";:,.!? "
+# _letters = "NQabdefghijklmnopstuvwxyzɑæʃʑçɯɪɔɛɹðəɫɥɸʊɾʒθβŋɦ⁼ʰ`^#*=ˈˌ→↓↑ "
 
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ" # !
+_letters_ipa = [
+    "a",
+    "b",
+    "tʃ",
+    "d",
+    "e",
+    "f",
+    "ɡ",
+    "h",
+    "i",
+    "dʒ",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "j",
+    "z",
+    "ŋ",
+    "ə",
+    "ɲ",
+    "ʃ",
+    "x",
+    "ʔ",
+]
 
-
-'''# shanghainese_cleaners
+"""# shanghainese_cleaners
 _pad        = '_'
 _punctuation = ',.!?…'
 _letters = 'abdfghiklmnopstuvyzøŋȵɑɔɕəɤɦɪɿʑʔʰ̩̃ᴀᴇ15678 '
-'''
+"""
 
-'''# chinese_dialect_cleaners
+"""# chinese_dialect_cleaners
 _pad        = '_'
 _punctuation = ',.!?~…─'
 _letters = '#Nabdefghijklmnoprstuvwxyzæçøŋœȵɐɑɒɓɔɕɗɘəɚɛɜɣɤɦɪɭɯɵɷɸɻɾɿʂʅʊʋʌʏʑʔʦʮʰʷˀː˥˦˧˨˩̥̩̃̚ᴀᴇ↑↓∅ⱼ '
-'''
+"""
 
-'''
+"""
 # Russian - from https://github.com/FENRlR/MB-iSTFT-VITS2/issues/2
 _pad = '_'
 _punctuation = ' !+,-.:;?«»—'
 _letters = 'абвгдежзийклмнопрстуфхцчшщъыьэюяё'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-'''
+"""
 
 # Export all symbols:
-#symbols = [_pad] + list(_punctuation) + list(_letters)
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) # !
+# symbols = [_pad] + list(_punctuation) + list(_letters)
+symbols = [_pad] + list(_punctuation) + _letters_ipa
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
